4e5f0efcc07e39bc974798e718074bfe7d340b8b
Nicolas Gicquel <ngicquel@arinfo.fr>
2022-01-14 09:43:16 +0100

    update products create form

--- products/index.php
+++ products/index.php
@@ -19,22 +19,28 @@ $categories = $req2->fetchAll(PDO::FETCH_ASSOC);
 
 <div class="card grid-margin">
     <div class="card-body">
-        <form class="form-group" action="createForm.php" method="post" enctype="multipart/form-data">
-            <input type="text" name="nameProduct" placeholder="Libellé du produit">
-            <select name="categoryId">
-                <?php
-                foreach ($categories as $key => $value) { ?>
-                    <option value="<?= $value['categoryId'] ?>"><?= $value['nameCategory'] ?></option>
-                <?php
-                }
-                ?>
+        <form class="form-group row" action="createForm.php" method="post" enctype="multipart/form-data">
+            <div class="col-lg-4 productForm">
+                <input type="text" name="nameProduct" placeholder="Libellé du produit">
+                <select name="categoryId">
+                    <?php
+                    foreach ($categories as $key => $value) { ?>
+                        <option value="<?= $value['categoryId'] ?>"><?= $value['nameCategory'] ?></option>
+                    <?php
+                    }
+                    ?>
 
-            </select>
-            <input type="number" step="0.01" name="price" placeholder="Prix du produit">
-            <input type="number" name="stockProduct" placeholder="Produits en stock">
-            <input type="file" name="imageProduct">
-            <textarea name="descriptionProduct" cols="30" rows="10">Description du produit</textarea>
-            <input type="submit" value="Créer un nouveau produit">
+                </select>
+                <input type="file" name="imageProduct">
+            </div>
+            <div class="col-lg-4 productForm">
+                <input type="number" step="0.01" name="price" placeholder="Prix du produit">
+                <input type="number" name="stockProduct" placeholder="Produits en stock">
+            </div>
+            <div class="col-lg-4">
+                <textarea name="descriptionProduct" cols="30" rows="10">Description du produit</textarea>
+            </div>
+            <input class="productSubmit nav-link btn btn-success create-new-button" type="submit" value="Créer un nouveau produit">
         </form>
     </div>
 </div>
@@ -61,10 +67,12 @@ $categories = $req2->fetchAll(PDO::FETCH_ASSOC);
                         <td class="actionTable"><a href="update.php?id=<?= $value['idProduct'] ?>"><i class="mdi mdi-update"></i></a><a href="delete.php?id=<?= $value['idProduct'] ?>"><i class="mdi mdi-delete"></a></td>
                     </tr>
                     <tr>
-                        <td colspan="6"><h5>Description Détaillée</h3></td>
+                        <td colspan="6">
+                            <h5>Description Détaillée</h3>
+                        </td>
                     </tr>
                     <tr class="border-bottom">
-                    <td class="alignLeft" colspan="6"><?= $value['descriptionProduct'] ?></td>
+                        <td class="alignLeft" colspan="6"><?= $value['descriptionProduct'] ?></td>
                     </tr>
                 <?php
                 }
